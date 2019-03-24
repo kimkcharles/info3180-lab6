889071f4843249cc3274331473e014ecb5839ed5
Kimberly Charles <kimberly.charles@mymona.uwi.edu>
2019-03-24 06:35:00 +0000

    Search portion working

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -42,31 +42,34 @@ Vue.component('app-footer', {
 
 Vue.component('news-list', {
     template: `
-        
-        <div class="form-inline d-flex justify-content-center">
-            <div class="form-group mx-sm-3 mb-2">
-                <label class="sr-only" for="search">Search</label>
-                <input type="search" name="search" v-model="searchTerm" id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
-                <button class="btn btn-primary mb-2" @click="searchNews">Search</button>
-            </div>
-        </div>
 
         
         <div class="news">
             <h2>News</h2>
+            
+            <div class="form-inline d-flex justify-content-center">
+                <div class="form-group mx-sm-3 mb-2">
+                    <label class="sr-only" for="search">Search</label>
+                    <input type="search" name="search" v-model="searchTerm" id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
+                    <button class="btn btn-primary mb-2" @click="searchNews">Search</button>
+                </div>
+            </div>
             <div class="news__list">
                 <div v-for="article in articles" class="news__item">
-                    <div class="news__title"><h6>{{ article.title }}</h6>
+                    <div class="news__title">
+                        <h6>{{ article.title }}</h6>
                     </div>
                     <div  class="news__photo">
-                    <img :src = article.urlToImage alt = 'Image for news article'>
+                        <img :src = article.urlToImage alt = 'Image for news article'>
                     </div>
                     <div  class="news__desc">
-                    {{ article.description }}
+                        {{ article.description }}
                     </div>
                 </div>
             </div>
         </div>
+        
+        
 
         `,
     
